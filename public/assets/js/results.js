5194681e15cc683e16d05fc8a5a0bc67a23db333
Austin Koeppel <Austin.Koeppel@gmail.com>
2018-06-27 23:21:56 -0400

    [Austin Koeppel][Initial front end js code]

---
+++ public/assets/js/results.js
@@ -0,0 +1,36 @@
+//Initialize all of our fields
+function init() {
+    document.getElementById("filterSubmit").onclick = clickboy;
+}
+
+//good ol' click boy
+function clickboy(e) {
+    e.preventDefault();
+    let subject = document.getElementById("subjectSelect").value;
+    let grade = document.getElementById("gradeSelect").value;
+    /*
+        AND THIS IS WHERE I WOULD PUT MY STAR RATINGS IF I HAD ANY
+        let stars = document.getElementById("starSelect").value;
+    */
+    submitSearch(grade,subject);
+}
+
+//actual ajax to submit to the backend
+function submitSearch(grade,subject,stars) {
+    let searchQuery = {
+        grade: grade,
+        subject: subject,
+        stars: stars
+    }
+    $.ajax("/api/search/" + id, {
+        type: "POST",
+        data: searchQuery
+      }).then(
+        function() {
+          console.log("SUCCESS");
+        }
+    );
+}
+
+//Call init at the end to make sure e'rything been hoisted
+init();
