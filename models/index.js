826615f21f59547e24c792538da9c40a4407c14f
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-06-30 12:07:44 -0400

    fix model for heroku

--- models/index.js
+++ models/index.js
@@ -1,27 +1,35 @@
-'use strict';
+"use strict";
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var basename = path.basename(module.filename);
+var env = process.env.NODE_ENV || "development";
 let keys = require(__dirname + "/../config/config.js");
 keys = keys.dbConfig[env];
-var db        = {};
+var db = {};
 
-if (keys.use_env_variable) {
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else if (keys.use_env_variable) {
   var sequelize = new Sequelize(process.env[keys.use_env_variable], keys);
 } else {
-  var sequelize = new Sequelize(keys.database, keys.username, keys.password, keys);
+  var sequelize = new Sequelize(
+    keys.database,
+    keys.username,
+    keys.password,
+    keys
+  );
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
   .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
+    var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
 
