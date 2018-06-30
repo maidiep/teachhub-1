b17ffd016f231cff050cd63dcc63ba1a0a2cb45b
Austin Koeppel <Austin.Koeppel@gmail.com>
2018-06-30 11:44:17 -0400

    [Just syncing up]

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/apiRoutes");
-<<<<<<< HEAD
-require("dotenv").load();
-=======
->>>>>>> 13e305cb4dc75e1fdd337a0143effa1e096a893f
 
 var app = express();
 var PORT = process.env.PORT || 3000;
