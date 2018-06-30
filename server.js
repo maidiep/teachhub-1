a780caa30c368f5f9a83cea6f1fbd52b9a3eb7ac
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-06-30 11:11:19 -0400

    port number change

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var routes = require("./routes/apiRoutes");
-require('dotenv').load();
+require("dotenv").load();
 
 var app = express();
-var PORT = process.env.PORT || 3060;
+var PORT = process.env.PORT || 3000;
 
 var db = require("./models");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
-
 
 require("./routes/apiRoutes")(app);
 const exphbs = require("express-handlebars");
@@ -21,8 +20,8 @@ app.set("view engine", "handlebars");
 
 //don't forget to put routes here
 
-db.sequelize.sync({force:true}).then(function() {
-    app.listen(PORT, function() {
-        console.log("App listening on port " + PORT);
-    });
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on port " + PORT);
+  });
 });
