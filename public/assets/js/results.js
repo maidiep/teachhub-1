92ae50c873c2a8c169633ffaf4e9cac72b1c3745
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-07-05 13:04:02 -0400

    fixing vanilla js.. chaged to query...

--- public/assets/js/results.js
+++ public/assets/js/results.js
@@ -1,34 +1,32 @@
 window.onload = init;
 //Initialize all of our fields
 function init() {
-    document.getElementById("filterSubmit").onclick = clickboy;
+  document.getElementById("filterSubmit").onclick = clickboy;
 }
 
 //good ol' click boy
 function clickboy(e) {
-    e.preventDefault();
-    let subject = document.getElementById("subjectSelect").value;
-    let grade = document.getElementById("gradeSelect").value;
-    /*
+  e.preventDefault();
+  let subject = $("#subjectSelect").val();
+  let grade = $("#gradeSelect").val();
+  /*
         AND THIS IS WHERE I WOULD PUT MY STAR RATINGS IF I HAD ANY
         let stars = document.getElementById("starSelect").value;
     */
-    submitSearch(grade,subject);
+  submitSearch(grade, subject);
 }
 
 //actual ajax to submit to the backend
-function submitSearch(grade,subject,stars) {
-    let searchQuery = {
-        grade: grade,
-        subject: subject,
-        stars: stars
-    }
-    $.ajax("/api/search/" + id, {
-        type: "POST",
-        data: searchQuery
-      }).then(
-        function() {
-          console.log("SUCCESS");
-        }
-    );
+function submitSearch(grade, subject, stars) {
+  let searchQuery = {
+    grade: grade,
+    subject: subject,
+    stars: stars
+  };
+  $.ajax("/api/search/" + id, {
+    type: "POST",
+    data: searchQuery
+  }).then(function() {
+    console.log("SUCCESS");
+  });
 }
